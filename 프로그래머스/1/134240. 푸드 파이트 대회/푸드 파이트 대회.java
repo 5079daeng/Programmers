8066fc99b0dae5f37b0316aa88e68faa05fb80f6
songhyun lee <94948667+5079daeng@users.noreply.github.com>
2025-03-10 21:15:28 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 3.57 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,16 @@
+import java.util.*; 
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        
+       StringBuilder sb = new StringBuilder(); 
+        for(int i = 1; i < food.length; i ++) {
+            sb.append(String.valueOf(i).repeat(food[i]/2));
+        }
+        
+        answer = sb.toString();
+        answer += "0";
+        answer += sb.reverse(); 
+        return answer;
+    }
+}
